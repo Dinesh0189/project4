2beff4e6d1e0cdd45e0ef41af3af8ae0261022f2
Dinesh0189 <dinesh06082005@gmail.com>
2026-02-23 13:57:24 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import useUsers from "./hooks/useUsers";
+import { Toaster } from "react-hot-toast";
+
+export default function App() {
+  const { users, isLoading } = useUsers();
+
+  return (
+    <div style={{ padding: 40 }}>
+      <Toaster position="top-right" reverseOrder={false} />
+      <h1>Axios Instance Demo</h1>
+      <h3>User List :</h3>
+
+      {isLoading ? (
+        <ul>
+          {[1, 2, 3, 4, 5].map((item) => (
+            <li
+              key={item}
+              style={{
+                background: "#cd1fa7ff",
+                height: "20px",
+                width: "200px",
+                margin: "10px 0",
+                borderRadius: "4px",
+              }}
+            ></li>
+          ))}
+        </ul>
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
